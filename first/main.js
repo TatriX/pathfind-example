4e8e4d9acaab45354effaaf4075f8cc82dc103f8
TatriX <tatrics@gmail.com>
2015-06-05 16:23:46 +0300

    Implemented subdivision

--- first/main.js
+++ first/main.js
@@ -1,6 +1,7 @@
 /*
  * TODO:
  * use priority queue instead of stupid loop
+ * make nodes subdivision when adding obstacles
  */
 
 var tileW = 32;
@@ -12,7 +13,7 @@ var numObstacles = 8;
 
 var controls = {
     showEdges: document.getElementById("show-edges"),
-    randomStart: document.getElementById("random-start"),
+    randomStartGoal: document.getElementById("random-start-goal"),
     go: document.getElementById("go"),
 };
 
@@ -21,10 +22,6 @@ function Node(x, y, w, h) {
     this.y = y;
     this.w = w;
     this.h = h;
-
-    this.centerX = this.x + this.w/2;
-    this.centerY = this.y + this.h/2;
-
     this.neighbors = [];
 
     this.parent = null;
@@ -39,11 +36,15 @@ Node.prototype = {
     get key() {
         return this.x + "," + this.y;
     },
+    equals: function(node) {
+        var e = 1e-6;
+        return Math.abs(this.x - node.x) < e && Math.abs(this.y - node.y) < e;
+    },
     link: function(node) {
         if (this.neighbors.indexOf(node) != -1)
             return;
         this.neighbors.push(node);
-        node.neighbors.push(this);
+        node.link(this);
     },
     detach: function() {
         // we need to copy neighbors via slice() because
@@ -51,27 +52,26 @@ Node.prototype = {
         this.neighbors.slice(0).forEach(this.unlink.bind(this));
     },
     unlink: function(node) {
-        this.neighbors.splice(this.neighbors.indexOf(node), 1);
-        node.neighbors.splice(node.neighbors.indexOf(this), 1);
+        var i = this.neighbors.indexOf(node);
+        if (i == -1)
+            return;
+        this.neighbors.splice(i, 1);
+        node.unlink(this);
     },
     distanceTo: function(node) {
-        var dx = this.centerX - node.centerX;
-        var dy = this.centerY - node.centerY;
+        var dx = this.x - node.x;
+        var dy = this.y - node.y;
         return Math.hypot(dx, dy);
     },
     manhattenDistanceTo: function(node) {
-        return Math.abs(this.centerX - node.centerX) + Math.abs(this.centerY - node.centerY);
+        return Math.abs(this.x - node.x) + Math.abs(this.y - node.y);
     },
     costTo: function(node) {
         return this.manhattenDistanceTo(node);
     },
     intersects: function(node) {
-        return this.x < node.x + node.w && this.y < node.y + node.h &&
-	    this.x + this.w > node.x && this.y + this.h > node.y;
-    },
-    touches: function(node) {
-        return this.x <= node.x + node.w && this.y <= node.y + node.h &&
-	    this.x + this.w >= node.x && this.y + this.h >= node.y;
+        return this.x < node.x+node.w && this.y < node.y+node.h &&
+	    this.x+this.w > node.x && this.y+this.h > node.y;
     },
     path: function() {
         var path = [this];
@@ -96,20 +96,20 @@ Node.prototype = {
         this.neighbors.forEach(function(node) {
             ctx.strokeStyle = "#ccc";
             ctx.beginPath();
-            ctx.moveTo(this.centerX, this.centerY);
-            ctx.lineTo(node.centerX, node.centerY);
+            ctx.moveTo(this.x + this.w/2, this.y + this.h/2);
+            ctx.lineTo(node.x + node.h/2, node.y + node.h/2);
             ctx.stroke();
 
             ctx.fillStyle = "#333";
             ctx.beginPath();
-            ctx.arc(node.centerX, node.centerY, 2, 0, 2*Math.PI);
+            ctx.arc(node.x + node.h/2, node.y + node.h/2, 2, 0, 2*Math.PI);
             ctx.fill();
         }.bind(this));
     }
 };
 
 function astar(start, goal) {
-    if (start.intersects(goal))
+    if (start.equals(goal))
         return [];
 
     var openset = {};
@@ -125,7 +125,7 @@ function astar(start, goal) {
                 current = openset[i];
         }
 
-        if (current.intersects(goal))
+        if (current.equals(goal))
             return current.path();
 
         delete openset[current.key];
@@ -176,7 +176,7 @@ Graph.prototype = {
                         node.link(this.nodes[i-mapSide+1]);
                 }
 
-                this.add(node);
+                this.nodes.push(node);
                 i++;
             }
         }
@@ -190,17 +190,11 @@ Graph.prototype = {
                 var first = this.nodes[0];
                 ctx.strokeStyle = "#f00";
                 ctx.beginPath();
-                ctx.moveTo(first.centerX, first.centerY);
+                ctx.moveTo(first.x + first.w/2, first.y + first.h/2);
                 this.nodes.forEach(function(node) {
-                    ctx.lineTo(node.centerX, node.centerY);
+                    ctx.lineTo(node.x + node.h/2, node.y + node.h/2);
                 });
                 ctx.stroke();
-                this.nodes.forEach(function(node) {
-                    ctx.fillStyle = "#f00";
-                    ctx.beginPath();
-                    ctx.arc(node.centerX, node.centerY, 3, 0, 2*Math.PI);
-                    ctx.fill();
-                });
             },
         };
         return this.path;
@@ -219,100 +213,12 @@ Graph.prototype = {
 
         this.path.draw();
     },
-    subdivide: function(obstacle) {
-        var nodes = this.findNodes(obstacle);
-        // make 4 corners nodes
-        // [nw]***[ne]
-        //  *       *
-        // [sw]***[se]
-        var max = {x: -Infinity, y: -Infinity};
-        var min = {x: +Infinity, y: +Infinity};
-        nodes.forEach(function(node) {
-            if (node.x + node.w > max.x)
-                max.x = node.x + node.w;
-            if (node.y + node.h > max.y)
-                max.y = node.y + node.h;
-
-            if (node.x < min.x)
-                min.x = node.x;
-            if (node.y < min.y)
-                min.y = node.y;
-        });
-
-        var nw = new Node(min.x, min.y, obstacle.x - min.x, obstacle.y - min.y);
-
-        var width = max.x - (obstacle.x + obstacle.w);
-        var ne = new Node(max.x - width, min.y, width, nw.h);
-
-        var height = max.y - (obstacle.y + obstacle.h);
-        var sw = new Node(min.x, max.y - height, nw.w, height);
-
-        var se = new Node(ne.x, sw.y, ne.w, sw.h);
-
-        // next
-        //  ***[n]***
-        //  *       *
-        // [w]     [e]
-        //  *       *
-        //  ***[s]***
-
-        width = (max.x - min.x) - (nw.w + ne.w);
-        var n = new Node(nw.x + nw.w, nw.y, width, ne.h);
-
-        height = (max.y - min.y) - (nw.h + sw.h);
-        var w = new Node(nw.x, nw.y + nw.h, nw.w, height);
-
-        var s = new Node(sw.x + sw.w, sw.y, width, sw.h);
-
-        var e = new Node(se.x, w.y, ne.w, height);
-
-        var subnodes = [
-            nw, n, ne,
-            w, /**/, e,
-            sw, s, se,
-        ];
-        w.link(nw);
-        w.link(sw);
-        e.link(ne);
-        e.link(se);
-        n.link(nw);
-        n.link(ne);
-        s.link(sw);
-        s.link(se);
-
-        nodes.forEach(function(node) {
-            subnodes.forEach(function(subnode) {
-                if (node.intersects(subnode)) {
-                    node.neighbors.forEach(function(neighbor) {
-                        if (nodes.indexOf(neighbor) != -1)
-                            return;
-                        if (subnode.touches(neighbor)) {
-                            subnode.link(neighbor);
-                        }
-                    });
-                }
-            });
-            graph.remove(node);
-        });
-
-        subnodes.forEach(function(subnode) {
-            if (subnode.w == 0 || subnode.h == 0)
-                subnode.detach();
-            else
-                this.add(subnode);
-        }.bind(this));
-    },
-    add: function(node) {
-        this.nodes.push(node);
-    },
-    remove: function(node) {
-        node.detach();
-        var i = this.nodes.indexOf(node);
-        this.nodes.splice(i, 1);
-    },
     addObstacle: function(obstacle) {
         this.obstacles.push(obstacle);
-        this.subdivide(obstacle);
+        this.findNodes(obstacle).forEach(function(node) {
+            node.color = "rgba(255, 0, 0, 0.5)";
+            node.detach();
+        });
     },
     findNodes: function(obstacle) {
         return this.nodes.filter(function(node) {
@@ -323,7 +229,7 @@ Graph.prototype = {
 
 function Obstacle(x, y, w, h) {
     var node = new Node(x, y, w, h);
-    node.color = "rgba(0, 0, 0, 0.5)";
+    node.color = "#555";
     return node;
 }
 
@@ -339,38 +245,27 @@ function rand(min, max) {
 
 var graph = new Graph();
 var path = [];
-var start = null;
-var goal = null;
-
 function go() {
     graph.init();
     generateObstacles();
-    initStartAndGoal();
-    graph.findPath(start, goal);
+    findPath();
     redraw();
 }
 
 function generateObstacles() {
-    for (var i = 0; i < numObstacles; ) {
+    for (var i = 0; i < numObstacles; i++) {
         var w = rand(8, 3*tileW);
         var h = rand(8, 3*tileH);
         var x = rand(0, mapSide*tileW - w);
         var y = rand(0, mapSide*tileH - h);
-        var obstacle = new Obstacle(x, y, w, h);
-        if (graph.obstacles.some(function(ob) {
-            return obstacle.intersects(ob);
-        })) {
-            continue;
-        }
-        graph.addObstacle(obstacle);
-        i++;
+        graph.addObstacle(new Obstacle(x, y, w, h));
     }
 }
 
-function initStartAndGoal() {
-    if (controls.randomStart.checked) {
-        start = graph.nodes[rand(0, graph.nodes.length)];
-        goal = graph.nodes[rand(0, graph.nodes.length)];
+function findPath() {
+    if (controls.randomStartGoal.checked) {
+        var start = graph.nodes[rand(0, graph.nodes.length)];
+        var goal = graph.nodes[rand(0, graph.nodes.length)];
     } else {
         start = graph.nodes[0];
         goal = graph.nodes[graph.nodes.length-1];
@@ -378,6 +273,8 @@ function initStartAndGoal() {
 
     goal.color = "#0c0";
     start.color = "#f00";
+
+    graph.findPath(start, goal);
 }
 
 function redraw() {
