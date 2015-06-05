f5db64e8af0cc0d4641fa3cc82d1731d0925145d
TatriX <tatrics@gmail.com>
2015-06-05 13:39:39 +0300

    Added settings checkboxes; fixed bugs

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ var mapW = mapSide * tileW;
 var mapH = mapSide * tileH;
 var numObstacles = 8;
 
+var controls = {
+    showEdges: document.getElementById("show-edges"),
+    randomStartGoal: document.getElementById("random-start-goal"),
+    go: document.getElementById("go"),
+};
+
 function Node(x, y, w, h) {
     this.x = x;
     this.y = y;
@@ -39,6 +45,11 @@ Node.prototype = {
             return;
         this.neighbors.push(node);
         node.link(this);
+    },
+    detach: function() {
+        // we need to copy neighbors via slice() because
+        // unlink will remove elements from it
+        this.neighbors.slice(0).forEach(this.unlink.bind(this));
     },
     unlink: function(node) {
         var i = this.neighbors.indexOf(node);
@@ -71,12 +82,30 @@ Node.prototype = {
         }
         return path;
     },
-    draw: function(ctx) {
+    draw: function() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.w, this.h);
         ctx.strokeStyle = "#333";
         ctx.strokeRect(this.x, this.y, this.w, this.h);
+
+        // print some stuff
+        // ctx.fillStyle = "#000";
+        // ctx.fillText(this.key, this.x, this.y+this.h/2);
     },
+    drawEdges: function() {
+        this.neighbors.forEach(function(node) {
+            ctx.strokeStyle = "#ccc";
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.w/2, this.y + this.h/2);
+            ctx.lineTo(node.x + node.h/2, node.y + node.h/2);
+            ctx.stroke();
+
+            ctx.fillStyle = "#333";
+            ctx.beginPath();
+            ctx.arc(node.x + node.h/2, node.y + node.h/2, 2, 0, 2*Math.PI);
+            ctx.fill();
+        }.bind(this));
+    }
 };
 
 function astar(start, goal) {
@@ -104,12 +133,15 @@ function astar(start, goal) {
 
         current.visited = true;
         current.neighbors.forEach(function(neighbor) {
+            if (neighbor.visited)
+                return;
             var g = current.g + current.distanceTo(neighbor);
-            if (!neighbor.visited || g < neighbor.g) {
+            var inOpen = neighbor.key in openset;
+            if (!inOpen || g < neighbor.g) {
                 neighbor.parent = current;
                 neighbor.g = g;
                 neighbor.f = g + neighbor.costTo(goal);
-                if (!openset[neighbor.key]) {
+                if (!inOpen) {
                     openset[neighbor.key] = neighbor;
                     opensetLength++;
                 }
@@ -119,45 +151,73 @@ function astar(start, goal) {
     return [];
 }
 
-function Graph() {
-    this.nodes = [];
-    this.obstacles = [];
-    var i = 0;
-    for (var y = 0; y < mapW; y += tileH) {
-        for (var x = 0; x < mapW; x += tileW) {
-            var node = new Node(x, y, tileW, tileH);
-            if (x > 0)
-                node.link(this.nodes[i-1]);
-            if (y > 0) {
-                if (x > 0)
-                    node.link(this.nodes[i-mapSide-1]);
-
-                node.link(this.nodes[i-mapSide]);
-
-                if (x < mapSide)
-                    node.link(this.nodes[i-mapSide+1]);
-            }
-
-            this.nodes.push(node);
-            i++;
-        }
-    }
-}
+function Graph() {}
 
 Graph.prototype = {
-    draw: function(ctx) {
-        this.nodes.forEach(function(node) {
-            node.draw(ctx);
-        });
-        this.obstacles.forEach(function(obstacle) {
-            obstacle.draw(ctx);
-        });
+    nodes: [],
+    obstacles: [],
+    path: [],
+    init: function() {
+        this.nodes = [];
+        this.obstacles = [];
+        var i = 0;
+        for (var y = 0; y < mapW; y += tileH) {
+            for (var x = 0; x < mapW; x += tileW) {
+                var node = new Node(x, y, tileW, tileH);
+                if (x > 0)
+                    node.link(this.nodes[i-1]);
+                if (y > 0) {
+                    if (x > 0)
+                        node.link(this.nodes[i-mapSide-1]);
+
+                    node.link(this.nodes[i-mapSide]);
+
+                    if (x < mapW-tileW)
+                        node.link(this.nodes[i-mapSide+1]);
+                }
+
+                this.nodes.push(node);
+                i++;
+            }
+        }
+    },
+    findPath: function(start, goal) {
+        this.path = {
+            nodes: astar(start, goal),
+            draw: function() {
+                if (this.nodes.length == 0)
+                    return;
+                var first = this.nodes[0];
+                ctx.strokeStyle = "#f00";
+                ctx.beginPath();
+                ctx.moveTo(first.x + first.w/2, first.y + first.h/2);
+                this.nodes.forEach(function(node) {
+                    ctx.lineTo(node.x + node.h/2, node.y + node.h/2);
+                });
+                ctx.stroke();
+            },
+        };
+        return this.path;
+    },
+    draw: function() {
+        function draw(obj) {
+            obj.draw();
+        }
+        this.nodes.forEach(draw);
+        if (controls.showEdges.checked) {
+            this.nodes.forEach(function(node) {
+                node.drawEdges();
+            });
+        }
+        this.obstacles.forEach(draw);
+
+        this.path.draw();
     },
     addObstacle: function(obstacle) {
         this.obstacles.push(obstacle);
         this.findNodes(obstacle).forEach(function(node) {
             node.color = "rgba(255, 0, 0, 0.5)";
-            node.neighbors.forEach(node.unlink.bind(node));
+            node.detach();
         });
     },
     findNodes: function(obstacle) {
@@ -173,11 +233,9 @@ function Obstacle(x, y, w, h) {
     return node;
 }
 
-var canvas = document.createElement("canvas");
+var canvas = document.getElementById("canvas");
 canvas.width = mapW;
 canvas.height = mapH;
-document.body.appendChild(canvas);
-
 var ctx = canvas.getContext("2d");
 
 // rand returns random number in [min, max)
@@ -185,11 +243,16 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+var graph = new Graph();
+var path = [];
 function go() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    graph.init();
+    generateObstacles();
+    findPath();
+    redraw();
+}
 
-    var graph = new Graph();
-
+function generateObstacles() {
     for (var i = 0; i < numObstacles; i++) {
         var w = rand(8, 3*tileW);
         var h = rand(8, 3*tileH);
@@ -197,21 +260,35 @@ function go() {
         var y = rand(0, mapSide*tileH - h);
         graph.addObstacle(new Obstacle(x, y, w, h));
     }
-
-    var start = graph.nodes[rand(0, graph.nodes.length)];
-    start.color = "#f00";
-
-    var goal = graph.nodes[rand(0, graph.nodes.length)];
-    goal.color = "#0c0";
-
-    var path = astar(start, goal);
-    path.forEach(function(node) {
-        if (node != start && node != goal)
-            node.color = "rgba(0, 0, 0, 0.5)";
-    });
-    graph.draw(ctx);
-
 }
 
-document.body.onclick = go;
+function findPath() {
+    if (controls.randomStartGoal.checked) {
+        var start = graph.nodes[rand(0, graph.nodes.length)];
+        var goal = graph.nodes[rand(0, graph.nodes.length)];
+    } else {
+        start = graph.nodes[0];
+        goal = graph.nodes[graph.nodes.length-1];
+    }
+
+    goal.color = "#0c0";
+    start.color = "#f00";
+
+    graph.findPath(start, goal);
+}
+
+function redraw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    graph.draw();
+}
+
+
+for (var i in controls) {
+    var control = controls[i];
+    if (control.dataset.redrawOnly)
+        control.onclick = redraw;
+    else
+        control.onclick = go;
+}
+
 go();
