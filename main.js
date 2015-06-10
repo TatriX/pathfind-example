b5272a1b5379dfb2202869eb3e3b2d955946b81c
TatriX <tatrics@gmail.com>
2015-06-10 06:54:14 +0300

    Added [].find polyfill

--- main.js
+++ main.js
@@ -346,6 +346,30 @@ function findNodes(nodes, node) {
     });
 }
 
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
 function findNode(nodes, x, y) {
     return nodes.find(function(node) {
         return node.containsPoint(x, y);
